7bb381c25037447dc81f6531f8ef18f1068a25b5
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-03-07 03:08:31 -0500

    Push WIP floating foundations mod

---
+++ FloatingFoundations/Buildables/FloatingFoundation.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using FloatingFoundations.API;
+using Nautilus.Assets;
+using Nautilus.Assets.Gadgets;
+using Nautilus.Crafting;
+using Nautilus.Utility;
+using UnityEngine;
+using UWE;
+using Object = UnityEngine.Object;
+
+namespace FloatingFoundations.Buildables;
+
+public static class FloatingFoundation
+{
+    public static PrefabInfo Info { get; private set; } = PrefabInfo.WithTechType("FloatingFoundation");
+
+    public static void Register()
+    {
+        var prefab = new CustomPrefab(Info);
+        prefab.SetGameObject(BuildPrefab);
+        prefab.SetRecipe(new RecipeData(new Ingredient(TechType.Titanium, 2),
+            new Ingredient(TechType.Silicone, 2),
+            new Ingredient(TechType.Floater, 1)
+            ));
+        prefab.SetPdaGroupCategoryAfter(TechGroup.BasePieces, TechCategory.BasePiece, TechType.BaseFoundation);
+        prefab.Register();
+        FloatingBuildableUtils.RegisterBuildableAsFloating(Info.TechType, -0.17f);
+    }
+
+    private static IEnumerator BuildPrefab(IOut<GameObject> result)
+    {
+        var request =
+            PrefabDatabase.GetPrefabForFilenameAsync("Assets/Prefabs/Base/GeneratorPieces/BaseFoundationPiece.prefab");
+        yield return request;
+        if (!request.TryGetPrefab(out var baseFoundationPiece))
+        {
+            Plugin.Logger.LogError("Failed to load prefab with name BaseFoundationPiece!");
+            yield break;
+        }
+
+        var prefab = new GameObject(Info.ClassID);
+        prefab.SetActive(false);
+        
+        var foundationModel = Object.Instantiate(baseFoundationPiece, prefab.transform);
+        foundationModel.transform.localPosition = new Vector3(0, -1, 0);
+        foundationModel.transform.localRotation = Quaternion.identity;
+
+        var pillowPrefab = Object.Instantiate(Plugin.Bundle.LoadAsset<GameObject>("FoundationPillowsPrefab"));
+        
+        var pillows = Object.Instantiate(pillowPrefab, foundationModel.transform);
+        pillows.transform.localPosition = Vector3.zero;
+        pillows.transform.localRotation = Quaternion.identity;
+        
+        MaterialUtils.ApplySNShaders(pillows, 6f);
+        PrefabUtils.AddBasicComponents(prefab, Info.ClassID, Info.TechType, LargeWorldEntity.CellLevel.Global);
+        var constructable = PrefabUtils.AddConstructable(prefab, Info.TechType,
+            ConstructableFlags.Outside | ConstructableFlags.Rotatable, foundationModel);
+        constructable.forceUpright = true;
+        constructable.placeDefaultDistance = 10;
+        constructable.placeMinDistance = 3;
+        constructable.placeMaxDistance = 20;
+        constructable.allowedUnderwater = false;
+        
+        var seamothRequest = CraftData.GetPrefabForTechTypeAsync(TechType.Seamoth);
+        yield return seamothRequest;
+        var seamothRef = seamothRequest.GetResult();
+        
+        var seamothProxy = seamothRef.GetComponentInChildren<WaterClipProxy>();
+        
+        var clipProxy = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        clipProxy.transform.parent = prefab.transform;
+        clipProxy.transform.localScale = new Vector3(9.8f, 1, 9.8f);
+        clipProxy.transform.localPosition = new Vector3(0, 0.476f, 0);
+        Object.DestroyImmediate(clipProxy.GetComponent<Collider>());
+        var waterClip = clipProxy.gameObject.AddComponent<WaterClipProxy>();
+        waterClip.shape = WaterClipProxy.Shape.Box;
+        waterClip.clipMaterial = seamothProxy.clipMaterial;
+        clipProxy.layer = LayerID.BaseClipProxy;
+        prefab.AddComponent<FixClipProxyLayer>().clipProxy = clipProxy;
+
+        result.Set(prefab);
+    }
+
+    private class FixClipProxyLayer : MonoBehaviour, IConstructable
+    {
+        public GameObject clipProxy;
+        
+        private void Start()
+        {
+            FixLayer();
+        }
+
+        private void FixLayer()
+        {
+            clipProxy.layer = LayerID.BaseClipProxy;
+        }
+
+        public bool IsDeconstructionObstacle()
+        {
+            return false;
+        }
+
+        public bool CanDeconstruct([UnscopedRef] out string reason)
+        {
+            reason = null;
+            return true;
+        }
+
+        public void OnConstructedChanged(bool constructed)
+        {
+            FixLayer();
+            StartCoroutine(FixLayerNextFrame());
+        }
+
+        private IEnumerator FixLayerNextFrame()
+        {
+            yield return null;
+            FixLayer();
+        }
+    }
+}
---
+++ FloatingFoundations/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using FloatingFoundations.Buildables;
+using HarmonyLib;
+using Nautilus.Handlers;
+using Nautilus.Utility;
+using UnityEngine;
+
+namespace FloatingFoundations;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("com.snmodding.nautilus")]
+public class Plugin : BaseUnityPlugin
+{
+    public new static ManualLogSource Logger { get; private set; }
+
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+    
+    internal static AssetBundle Bundle { get; private set; }
+
+    private static bool _loadedAssets;
+
+    private void Awake()
+    {
+        // set project-scoped logger instance
+        Logger = base.Logger;
+
+        WaitScreenHandler.RegisterEarlyLoadTask(PluginInfo.PLUGIN_NAME, RegisterMod);
+
+        // register harmony patches, if there are any
+        Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private static void RegisterMod(WaitScreenHandler.WaitScreenTask task)
+    {
+        if (_loadedAssets) return;
+        
+        Bundle = AssetBundleLoadingUtils.LoadFromAssetsFolder(Assembly, "floatingfoundation");
+        FloatingFoundation.Register();
+        
+        _loadedAssets = true;
+    }
+}
